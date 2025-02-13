e2c32c6faf42d7728e6960b80a1d891725583810
itsogamers-bot1 <itsogamers.bot1@gmail.com>
2025-02-13 14:58:43 +0900

    2025-02-13-14-58

--- app/server.py
+++ app/server.py
@@ -9,6 +9,11 @@ app = FastAPI()
 async def root():
 	return {"message": "Server is Online."}
 
+# added for uptimerobot
+@app.head("/")
+async def head():
+    return {}  # No body needed for HEAD, only headers will be returned
+
 def start():
 	uvicorn.run(app, host="0.0.0.0", port=8080)
 
